6c0d0cf5ddbead543f915a89e814aa89f240577e
thanhhoai162963 <hoaithanh249@gmail.com>
2021-06-16 11:21:23 +0700

    demmo

--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var student: Student
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val student = Student("thánh")
+        binding.user1 = student
+
     }
 }
---
+++ app/src/main/java/com/example/databinding/Student.kt
@@ -0,0 +1,4 @@
+package com.example.databinding
+
+class Student(var name:String= "") {
+}
